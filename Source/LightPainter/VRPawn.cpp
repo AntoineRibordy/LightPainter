179de5e22d153d4239e1a963ea8778c57885343c
AntoineRibordy <antoine.ribordy@gmail.com>
2019-05-28 16:41:09 +0100

    feat: Create VR Pawn and handcontroller

---
+++ Source/LightPainter/VRPawn.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "VRPawn.h"
+#include "Engine/World.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+AVRPawn::AVRPawn()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	VRRoot = CreateDefaultSubobject<USceneComponent>(TEXT("VRRoot"));
+	VRRoot->SetupAttachment(GetRootComponent());
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(VRRoot);
+
+}
+
+// Called when the game starts or when spawned
+void AVRPawn::BeginPlay()
+{
+	Super::BeginPlay();
+
+	RightController = GetWorld()->SpawnActor<AHandController>(HandControllerClass);
+	if (RightController != nullptr)
+	{
+		RightController->AttachToComponent(VRRoot, FAttachmentTransformRules::KeepRelativeTransform);
+		RightController->SetHand(EControllerHand::Right);
+	}
+	
+}
+
+// Called every frame
+void AVRPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AVRPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
