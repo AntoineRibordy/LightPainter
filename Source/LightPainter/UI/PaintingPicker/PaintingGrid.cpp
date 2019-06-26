f2337dcdad26d7439cc71a2548b3d727da6a3954
AntoineRibordy <antoine.ribordy@gmail.com>
2019-06-26 13:59:37 +0100

    feat: Add delete and add buttons to saving UI + build skeleton to add saved games to widget

---
+++ Source/LightPainter/UI/PaintingPicker/PaintingGrid.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PaintingGrid.h"
+#include "Components/SizeBox.h"
+
+void UPaintingGrid::AddPainting()
+{
+	if (!PaintingGrid) return;
+
+	UUserWidget* NewWidget = CreateWidget<UUserWidget>(GetWorld(), GridCardClass);
+	if (!NewWidget) return;
+	USizeBox* FirstChild = Cast<USizeBox>(PaintingGrid->GetChildAt(0));
+	if (!FirstChild) return;
+	FirstChild->AddChild(NewWidget);
+}
